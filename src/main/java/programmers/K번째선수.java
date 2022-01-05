4705d081c60893b0be77114c67c7339de002f4ea
LeeSangBum <dltkdqja101@gmail.com>
2022-01-05 17:53:28 +0900

    프로그래머스    -신규 아이디 추천 12.19    -로또의 최고 순위와 최저 순위 12.21    -숫자 문자열과 영단어 12.22    -키패드 누르기 12.23 어려워서 다보고침 ...    -크레인 인형뽑기 게임 12.24 내일은 크리스마스다...    -크레인 입형뽑기 게임 12.24 입력값 5 초과일 경우 코드 삽입    -없는 숫자 더하기 12.27 쉬워서 좋아했는데 나만 쉬운게 아님    -음양 더하기 12.28 이번 문제도 쉬웠으나 나만 쉬운게 아님    -소수 만들기 12.30    -완주하지 못한 선수 12.31 다시풀기    -k 번째수 22.01.04 쉬우면서도 어려웠다.    -모의고사 22.01.05 왜 항상 30% 생각하고 뇌가 굳음

--- src/main/java/programmers/K번째선수.java
+++ src/main/java/programmers/K번째선수.java
@@ -1,11 +1,12 @@
 package programmers;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 public class K번째선수 {
 
     public static void main(String[] args) {
-        
+
         int[] array = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
         System.out.println("정렬배열 : " + Arrays.toString(solution(array, commands)));
@@ -21,13 +22,17 @@ public class K번째선수 {
         int[] result = new int[len];
 
         /**
-         * 기존 배열을 특정범위만 재생성 후 정렬 -> 정렬 된 배열의 특정 index위치 값 추출
+         * 기존 배열을 특정범위만 재생성 후 정렬 -> 정렬 된 배열의 특정 index 위치 값 추출
          */
         for(int i = 0; i < len; i++) {
             answer = Arrays.copyOfRange(array, commands[i][0] -1, commands[i][1]);
             Arrays.sort(answer);
             result[i] = answer[commands[i][2] - 1];
         }
+        /*
+        각 위치에 - 1 을 해준 이유는 배열은 0부터 시작하기 때문이며,
+        - 1 이 붙지 않은 Arrays 의 copyOfRange 메소드는 그 위치 값 이전 까지 추출하기 때문이다.
+        */
 
         return result;
     }
---
+++ src/main/java/programmers/모의고사.java
@@ -0,0 +1,54 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 모의고사 {
+
+    public int[] solution(int[] answers) {
+
+        //수포자 1, 2, 3 번의 정답 패턴
+        int[][] pattern = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+        //수포자 1, 2, 3 번의 정답 수
+        int[] score = new int[pattern.length];
+        //수포자 1, 2, 3 번의 정답 정렬
+        int[] scoreSort = new int[pattern.length];
+        //가장문제를 많이 맞춘 수포자
+        List<Integer> scoreRankList = new ArrayList<>();
+
+        //각각의 패턴을 정답과 비교하여 score 점수 증가
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == pattern[0][i % pattern[0].length]) score[0]++;
+            if (answers[i] == pattern[1][i % pattern[1].length]) score[1]++;
+            if (answers[i] == pattern[2][i % pattern[2].length]) score[2]++;
+        }
+
+        //배열 정렬을 위한 배열 복사
+        scoreSort = score.clone();
+        //가장 높은 점수를 추출하기 위한 정렬
+        Arrays.sort(scoreSort);
+        //오름차순 정렬로 가장 뒷 값이 최대값 / 최대 값이 2개 이상일 경우를 대비하여 비교변수 max 생성
+       int max = scoreSort[scoreSort.length - 1];
+
+        //가장문제를 많이 맞춘 학생 찾기 복수 일 경우를 대비하여 List 에 저장
+        for (int j = 0; j < score.length; j++) {
+            if (score[j] == max) scoreRankList.add(j+1);
+        }
+
+        //최다점 학생 수 만큼의 배열크기 생성
+        int[] result = new int[scoreRankList.size()];
+
+        //리스트의 최다점 학생들을 배열에 저장
+       for (int k = 0; k < result.length; k++) {
+            result[k] = scoreRankList.get(k);
+        }
+
+        return result;
+    }
+
+}//class
