82df969a7d65cfab85970ee5fe7528f0052422fd
LeeSangBum <dltkdqja101@gmail.com>
2022-01-16 19:36:59 +0900

    프로그래머스    -신규 아이디 추천 12.19    -로또의 최고 순위와 최저 순위 12.21    -숫자 문자열과 영단어 12.22    -키패드 누르기 12.23 어려워서 다보고침 ...    -크레인 인형뽑기 게임 12.24 내일은 크리스마스다...    -크레인 입형뽑기 게임 12.24 입력값 5 초과일 경우 코드 삽입    -없는 숫자 더하기 12.27 쉬워서 좋아했는데 나만 쉬운게 아님    -음양 더하기 12.28 이번 문제도 쉬웠으나 나만 쉬운게 아님    -소수 만들기 12.30    -완주하지 못한 선수 12.31 다시풀기    -k 번째수 22.01.04 쉬우면서도 어려웠다.    -모의고사 22.01.05 왜 항상 30% 생각하고 뇌가 굳음    -체육복 22.01.09 아직 1단계 어렵습니다.    -폰켓몬 22.01.11 혼자 풀었습니다. 남들은 더 쉽게 풀었습니다..    -실패율 22.01.12 수정 람다식 다시 공부하기

--- src/main/java/programmers/level2/실패율.java
+++ src/main/java/programmers/level2/실패율.java
@@ -1,31 +1,69 @@
 package programmers.level2;
 
+import java.util.*;
+import java.util.Map.Entry;
+
 public class 실패율 {
     public static void main(String[] args) {
         int N = 5;
-        int[] stages = {2, 1, 2, 5, 2, 4, 3, 3};
-        System.out.println("확룰 : " + solution(N, stages).toString());
-
+        int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
+        System.out.println("result : " + Arrays.toString(solution(N, stages)));
     }
 
-    //N = 스테이지 총 수, stages = 각 유저가 머물러 있는 스테이지
-    public static Double[][] solution(int N, int[] stages) {
-        //스테이지 도전 인원
-        int challengeUsers = stages.length;
-        //스테이지 별 확률을 담을 배열
-        Double[][] stageProbability = new Double[N][1];
-        //스테이지 별 확률을 구하자
-        for (int i = 1; i <= N; i++) {
+    public static int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
 
-            Double count = 0.0;
+        /*각 스테이지 별 도착인원 수 저장 key : stage, value : peopleNum*/
+        Map<Integer, Integer> stageMap = new HashMap<>();
+        /*스테이지 별 탈락율 저장 key : stage, value : 탈락율*/
+        Map<Integer, Double> failRateMap = new HashMap<>();
 
-            for (int j = 0; j < stages.length; j++)
-                if (stages[j] == i) count++;
-            stageProbability[i][1] = count / challengeUsers;
-            challengeUsers -= count;
-
+        /*각 스테이지 별 도착인원 수 저장*/
+        for (int i : stages) {
+            //stageMap i 번째에 stageMap i 번째의 value 를 넣을건데 없으면 0을 넣어라
+            stageMap.put(i, stageMap.getOrDefault(i, 0) + 1);
         }
-        return stageProbability;
+        //스테이지별 도전자 수 체크 초기값 1라운드는 전체 도전자 수
+        int challengeUsers = stages.length;
+        //현재 스테이지
+        int nowStage = 1;
+
+        //while 1단계 부터 최종단계 + 1(최종단계 클리어시 + 1 값이 저장됌) 까지 반복
+        while (nowStage <= N + 1) {
+            //key 와 value 사용을 위한 Entry 인터페이스 활용
+            for (Entry<Integer, Integer> e : stageMap.entrySet()) {
+                //각 스테이지 별로 차례대로 실패확율을 저장하기 위한 조건문
+                if (e.getKey() == nowStage) {
+                    //nowStage(현재스테이지), 현재스테이지의 값 / 도전자 수(실패확율 비교를 위한 실수형변환 계산)
+                    failRateMap.put(nowStage, e.getValue() / Double.valueOf(challengeUsers));
+                    //전체 도전자 수 - 스테이지에 남아있던 사람 수
+                    challengeUsers -= e.getValue();
+                    break;
+                }
+            }
+            //다음 단계로 이동
+            nowStage++;
+        }//while
+
+        //스테이지별 실패확율이 없는 스테이지는 0으로 처리 (ex:1,4,2,4,5)
+        for (int i = 1; i <= N; i++) {
+            failRateMap.put(i, failRateMap.getOrDefault(i, 0.0));
+        }
+        System.out.println("failRateMap.toString() : " + failRateMap.toString());
+
+        //마지막 스테이지 까지 도달한 유저는 실패율에서 제외
+        failRateMap.remove(N + 1);
+
+        List<Integer> keySetList = new ArrayList<>(failRateMap.keySet());
+        System.out.println("keySetList : " + keySetList.toString());
+        //실패율 내림차순 정렬
+        Collections.sort(keySetList, (o1, o2) -> (failRateMap.get(o2).compareTo(failRateMap.get(o1))));
+        System.out.println("keySetList2 : " + keySetList.toString());
+        System.out.println("failRateMap.get(1) : " +failRateMap.get(1));
+        //실패율 스테이지 저장
+        answer = keySetList.stream().mapToInt(i -> i).toArray();
+
+        return answer;
     }
 
-}
+}//class
