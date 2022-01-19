6132bc101a6d1ff38d1547c4a459008431ae5d76
LeeSangBum <dltkdqja101@gmail.com>
2022-01-19 20:05:17 +0900

    프로그래머스    -신규 아이디 추천 12.19    -로또의 최고 순위와 최저 순위 12.21    -숫자 문자열과 영단어 12.22    -키패드 누르기 12.23 어려워서 다보고침 ...    -크레인 인형뽑기 게임 12.24 내일은 크리스마스다...    -크레인 입형뽑기 게임 12.24 입력값 5 초과일 경우 코드 삽입    -없는 숫자 더하기 12.27 쉬워서 좋아했는데 나만 쉬운게 아님    -음양 더하기 12.28 이번 문제도 쉬웠으나 나만 쉬운게 아님    -소수 만들기 12.30    -완주하지 못한 선수 12.31 다시풀기    -k 번째수 22.01.04 쉬우면서도 어려웠다.    -모의고사 22.01.05 왜 항상 30% 생각하고 뇌가 굳음    -체육복 22.01.09 아직 1단계 어렵습니다.    -폰켓몬 22.01.11 혼자 풀었습니다. 남들은 더 쉽게 풀었습니다..    -실패율 22.01.12 수정 람다식 다시 공부하기    -약수의 개수와 덧셈 22.01.19

---
+++ src/main/java/programmers/level1/약수의개수와덧셈.java
@@ -0,0 +1,29 @@
+package programmers.level1;
+
+public class 약수의개수와덧셈 {
+
+    public static void main(String[] args) {
+        int left = 13;
+        int right = 17;
+        System.out.println("약수의개수와 덧셈뺄셈 : " + solution(left, right));
+    }
+
+    public static int solution(int left, int right) {
+        //결과값 변수
+        int result = 0;
+        //주어진 left 부터 right 까지 하나 씩 검사
+        for (int i = left; i <= right; i++) {
+            //약수의 개수를 담을 변수
+            int count = 0;
+            //left 부터 right 까지의 수를 하나 씩 1부터 해당 수까지 계산 후 count++
+            for (int j = 1; j <= i; j++) if (i % j == 0) count++;
+            //약수의 개수가 짝 / 홀 인지 검사 후 + -
+            if (count % 2 == 0) result += i;
+            else result -= i;
+        }
+
+        return result;
+
+    }//solution
+
+}//class
